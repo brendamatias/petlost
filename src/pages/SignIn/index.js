1c7114ca14f40ee7107bf61c20f0d6c2d1ac170e
brendamatias <brendamatias.sobral@gmail.com>
2020-04-10 01:14:51 -0300

    chore: using unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/petlost.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {}
+
   return (
     <>
       <img src={logo} alt="PetLost" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <h3>Log into your account</h3>
 
-        <label>Email Address</label>
-        <input type="email" placeholder="example@mail.com" />
+        <label htmlFor="email">Email Address</label>
+        <Input name="email" type="email" placeholder="example@mail.com" />
 
-        <label>Password</label>
-        <input type="password" placeholder="••••••••" className="password" />
+        <label htmlForm="password">Password</label>
+        <Input
+          name="password"
+          type="password"
+          placeholder="••••••••"
+          className="password"
+        />
         <span>Forgot Password?</span>
 
         <button type="submit">Log in</button>
         <Link to="/register">Dont have an account? Create an account</Link>
-      </form>
+      </Form>
 
       <span>©‎2020 PetLost Studio - Brazil</span>
     </>
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/petlost.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {}
+
   return (
     <>
       <img src={logo} alt="PetLost" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <h3>Create your account</h3>
 
-        <label>Full Name</label>
-        <input type="email" placeholder="Example Name" />
+        <label htmlFor="name">Full Name</label>
+        <Input name="name" type="email" placeholder="Example Name" />
 
-        <label>Email Address</label>
-        <input type="email" placeholder="example@mail.com" />
+        <label htmlFor="email">Email Address</label>
+        <Input name="email" type="email" placeholder="example@mail.com" />
 
-        <label>Password</label>
-        <input type="password" placeholder="••••••••" className="password" />
+        <label htmlFor="password">Password</label>
+        <Input
+          name="password"
+          type="password"
+          placeholder="••••••••"
+          className="password"
+        />
 
         <button type="submit">Register</button>
         <Link to="/">Already have an account? Please login.</Link>
-      </form>
+      </Form>
 
       <span>©‎2020 PetLost Studio - Brazil</span>
     </>
