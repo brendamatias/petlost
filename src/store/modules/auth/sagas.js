db81924f5f5138a45a9ce00e7627cad4b1ce37d1
brendamatias <brendamatias.sobral@gmail.com>
2020-04-10 22:16:53 -0300

    feat: user registration

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -31,4 +31,30 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    toast.success('Account created successfully!');
+
+    history.push('/');
+  } catch (err) {
+    const { response } = err;
+
+    toast.error(response.data.error || 'Registration failed, check your data.');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
