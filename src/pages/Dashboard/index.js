c1b62c61c669cd11f7e4596267d4eec61beadda3
brendamatias <brendamatias.sobral@gmail.com>
2020-04-12 17:38:19 -0300

    refactor: send new message

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import bcrypt from 'bcryptjs';
+import { toast } from 'react-toastify';
 
 // import { Container } from './styles';
 
-import { socket } from '~/services/api';
+import { api, socket } from '~/services/api';
 
 export default function Dashboard() {
   const profile = useSelector(state => state.user.profile);
 
   async function handleSubmit(id) {
-    // console.log(process.env.REACT_APP_API_KEY);
-    const apiKey = process.env.REACT_APP_API_KEY;
-    const secretText = profile.id + profile.name + profile.email + apiKey;
+    try {
+      const { data } = await api.get(`keys/${id}`);
 
-    const key = await bcrypt.hash(secretText, 8);
+      const messageObject = {
+        content: 'Oi, tudo bom?',
+        key: data.key,
+        sender: profile.id,
+        recipient: id,
+      };
 
-    const messageObject = {
-      content: 'Oi, tudo bom?',
-      key,
-      sender: profile.id,
-      recipient: id,
-    };
+      socket.emit('sendMessage', messageObject);
 
-    socket.emit('sendMessage', messageObject);
+      toast.success('Message sent successfully!');
+    } catch (err) {
+      const { response } = err;
+
+      toast.error(
+        response.data.error || 'Oops, server error. Try again later.'
+      );
+    }
   }
 
   return (
