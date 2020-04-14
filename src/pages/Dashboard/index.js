d6c70c913d0f9bba5a7f10bfd6c07690b0e181a4
brendamatias <brendamatias.sobral@gmail.com>
2020-04-13 22:20:47 -0300

    Revert "fix: test netlify"
    This reverts commit 2717bc3f6d2206824f8f3fb807a180d9e19a3b13.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,178 +1,159 @@
 import React, { useState } from 'react';
-// import { useSelector } from 'react-redux';
-// import { toast } from 'react-toastify';
+import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
-// import { MdLocationOn, MdFavorite } from 'react-icons/md';
-// import { Container, Content, Filters, Tags, Tag } from './styles';
+import { MdLocationOn, MdFavorite } from 'react-icons/md';
+import { Container, Content, Filters, Tags, Tag } from './styles';
 
-// import Filter from './Filter';
+import Filter from './Filter';
 
-// import { api, socket } from '~/services/api';
+import { api, socket } from '~/services/api';
 
-// const teste = [
-//   {
-//     name: 'Bolinha',
-//     img:
-//       'https://s2.glbimg.com/9rfjedp117RCh68P_rzWxcOQZUY=/e.glbimg.com/og/ed/f/original/2018/02/01/26907788_1614183255284072_4356465325754439623_n.jpg',
-//     filters: ['Cats', 'Adoption'],
-//   },
-//   {
-//     name: 'Cebolinha',
-//     img:
-//       'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/03/marnie-dog.jpg"',
-//     filters: ['Adoption', 'Dogs'],
-//   },
-//   {
-//     name: 'Monica',
-//     img:
-//       'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/03/marnie-dog.jpg"',
-//     filters: ['Mating', 'Dogs'],
-//   },
-//   {
-//     name: 'Cascão',
-//     img:
-//       'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/03/marnie-dog.jpg"',
-//     filters: ['Disappear', 'Dogs'],
-//   },
-// ];
+const pets = [
+  {
+    name: 'Bolinha',
+    img:
+      'https://s2.glbimg.com/9rfjedp117RCh68P_rzWxcOQZUY=/e.glbimg.com/og/ed/f/original/2018/02/01/26907788_1614183255284072_4356465325754439623_n.jpg',
+    filters: ['Adoption', 'Cats'],
+  },
+  {
+    name: 'Cebolinha',
+    img:
+      'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/03/marnie-dog.jpg"',
+    filters: ['Adoption', 'Dogs'],
+  },
+  {
+    name: 'Monica',
+    img:
+      'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/03/marnie-dog.jpg"',
+    filters: ['Mating', 'Dogs'],
+  },
+  {
+    name: 'Cascão',
+    img:
+      'https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2020/03/marnie-dog.jpg"',
+    filters: ['Disappear', 'Dogs'],
+  },
+];
 
 export default function Dashboard() {
-  //   const [adoption, setAdoption] = useState(false);
-  //   const [cats, setCats] = useState(false);
-  //   const [disappear, setDisappear] = useState(false);
-  //   const [dogs, setDogs] = useState(false);
-  //   const [mating, setMating] = useState(false);
-  //   const [pets, setPets] = useState(teste);
+  const [adoption, setAdoption] = useState(false);
+  const [cats, setCats] = useState(false);
+  const [disappear, setDisappear] = useState(false);
+  const [dogs, setDogs] = useState(false);
+  const [mating, setMating] = useState(false);
 
-  //   const [filters, setFilters] = useState(teste);
+  const profile = useSelector(state => state.user.profile);
 
-  //   const profile = useSelector(state => state.user.profile);
+  function returnColor(filter) {
+    if (filter === 'Adoption') {
+      return '#20BAA3';
+    }
 
-  //   function filterData(filter, setFilter, content) {
-  //     const data = filters.filter(pet => {
-  //       for (let i = 0; i < pet.filters.length; i++) {
-  //         if (pet.filters[i] === content) {
-  //           console.log(filter);
-  //           return !filter;
-  //         }
-  //       }
-  //     });
+    if (filter === 'Cats') {
+      return '#4501BD';
+    }
 
-  //     setFilter(!filter);
+    if (filter === 'Disappear') {
+      return '#1066BD';
+    }
 
-  //     setPets(data);
-  //   }
+    if (filter === 'Dogs') {
+      return '#C24F00';
+    }
 
-  //   function returnColor(filter) {
-  //     if (filter === 'Adoption') {
-  //       return '#20BAA3';
-  //     }
+    return '#BA2929';
+  }
 
-  //     if (filter === 'Cats') {
-  //       return '#4501BD';
-  //     }
+  async function handleSubmit(id) {
+    try {
+      const { data } = await api.get(`keys/${id}`);
 
-  //     if (filter === 'Disappear') {
-  //       return '#1066BD';
-  //     }
+      const messageObject = {
+        content: 'Oi, tudo bom?',
+        key: data.key,
+        sender: profile.id,
+        recipient: id,
+      };
 
-  //     if (filter === 'Dogs') {
-  //       return '#C24F00';
-  //     }
+      socket.emit('sendMessage', messageObject);
 
-  //     return '#BA2929';
-  //   }
+      toast.success('Message sent successfully!');
+    } catch (err) {
+      const { response } = err;
 
-  //   async function handleSubmit(id) {
-  //     try {
-  //       const { data } = await api.get(`keys/${id}`);
-
-  //       const messageObject = {
-  //         content: 'Oi, tudo bom?',
-  //         key: data.key,
-  //         sender: profile.id,
-  //         recipient: id,
-  //       };
-
-  //       socket.emit('sendMessage', messageObject);
-
-  //       toast.success('Message sent successfully!');
-  //     } catch (err) {
-  //       const { response } = err;
-
-  //       toast.error(
-  //         response.data.error || 'Oops, server error. Try again later.'
-  //       );
-  //     }
-  //   }
+      toast.error(
+        response.data.error || 'Oops, server error. Try again later.'
+      );
+    }
+  }
 
   return (
-    <> </>
-    // <Container>
-    //   <h1>Dashboard</h1>
+    <Container>
+      <h1>Dashboard</h1>
 
-    //   <Content>
-    //     <Filters>
-    //       <Filter
-    //         active={adoption}
-    //         handleSubmit={() => {
-    //           filterData(adoption, setAdoption, 'Adoption');
-    //         }}
-    //         content="Adoption"
-    //       />
-    //       <Filter
-    //         active={cats}
-    //         handleSubmit={() => {
-    //           setCats(!cats);
-    //         }}
-    //         content="Cats"
-    //       />
-    //       <Filter
-    //         active={disappear}
-    //         handleSubmit={() => {
-    //           setDisappear(!disappear);
-    //         }}
-    //         content="Disappear"
-    //       />
-    //       <Filter
-    //         active={dogs}
-    //         handleSubmit={() => {
-    //           setDogs(!dogs);
-    //         }}
-    //         content="Dogs"
-    //       />
-    //       <Filter
-    //         active={mating}
-    //         handleSubmit={() => {
-    //           setMating(!mating);
-    //         }}
-    //         content="Mating"
-    //       />
-    //     </Filters>
+      <Content>
+        <Filters>
+          <Filter
+            active={adoption}
+            handleSubmit={() => {
+              setAdoption(!adoption);
+            }}
+            content="Adoption"
+          />
+          <Filter
+            active={cats}
+            handleSubmit={() => {
+              setCats(!cats);
+            }}
+            content="Cats"
+          />
+          <Filter
+            active={disappear}
+            handleSubmit={() => {
+              setDisappear(!disappear);
+            }}
+            content="Disappear"
+          />
+          <Filter
+            active={dogs}
+            handleSubmit={() => {
+              setDogs(!dogs);
+            }}
+            content="Dogs"
+          />
+          <Filter
+            active={mating}
+            handleSubmit={() => {
+              setMating(!mating);
+            }}
+            content="Mating"
+          />
+        </Filters>
 
-    //     <ul>
-    //       {pets.map(pet => (
-    //         <li>
-    //           <button type="submit">
-    //             <MdFavorite size={15} />
-    //           </button>
-    //           <img src={pet.img} alt="" />
-    //           <div>
-    //             <Tags>
-    //               {pet.filters.map(filter => (
-    //                 <Tag color={returnColor(filter)}>{filter}</Tag>
-    //               ))}
-    //             </Tags>
-    //             <strong>{pet.name}</strong>
-    //             <p>
-    //               <MdLocationOn color="#bb2929" size={18} />
-    //               Recife - PE <strong>(3.6 km)</strong>
-    //             </p>
-    //           </div>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </Content>
-    // </Container>
+        <ul>
+          {pets.map(pet => (
+            <li>
+              <button type="submit">
+                <MdFavorite size={15} />
+              </button>
+              <img src={pet.img} alt="" />
+              <div>
+                <Tags>
+                  {pet.filters.map(filter => (
+                    <Tag color={returnColor(filter)}>{filter}</Tag>
+                  ))}
+                </Tags>
+                <strong>{pet.name}</strong>
+                <p>
+                  <MdLocationOn color="#bb2929" size={18} />
+                  Recife - PE <strong>(3.6 km)</strong>
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Content>
+    </Container>
   );
 }
