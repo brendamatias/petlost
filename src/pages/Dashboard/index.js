fc4c2259f0127a8ae709c4d59f0b0c7331d227ce
brendamatias <brendamatias.sobral@gmail.com>
2021-02-01 01:48:59 -0300

    refactor pet list

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,57 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 
-import { MdLocationOn, MdFavorite } from 'react-icons/md';
-import { Container, Content, TagsFilters, TagFilter } from './styles';
+import { Container, Content } from './styles';
 
 import Loading from '~/components/Loading';
-import MultiSelect from '~/components/MultiSelect';
+import Header from '~/components/Header';
+import PetsList from '~/components/PetsList';
 
 import { api } from '~/services/api';
-
-import noImage from '~/assets/no-image.png';
 
 export default function Dashboard() {
   const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [filters, setFilters] = useState([]);
   const [pets, setPets] = useState([]);
-
-  function returnColor(filter) {
-    if (filter === 'dog') return '#C24F00';
-    if (filter === 'cat') return '#4501BD';
-    if (filter === 'mating') return '#BA2929';
-    if (filter === 'disappeared') return '#1066BD';
-    if (filter === 'adoption') return '#20BAA3';
-
-    return '#000';
-  }
-
-  function returnTraduction(filter) {
-    if (filter === 'dog') return 'cachorro';
-    if (filter === 'cat') return 'gato';
-    if (filter === 'mating') return 'acasalamento';
-    if (filter === 'disappeared') return 'desaparecido';
-    if (filter === 'adoption') return 'adoção';
-
-    return '';
-  }
-
-  function onChangeFilters(values) {
-    let filterParams = '';
-
-    values.map(filter => {
-      if (filter.value === 'dog' || filter.value === 'cat') {
-        filterParams = `${filterParams}&type=${filter.value}`;
-      } else {
-        filterParams = `${filterParams}&situation=${filter.value}`;
-      }
-
-      return filterParams;
-    });
-
-    setFilters(filterParams ? `?${filterParams}` : '');
-  }
 
   useEffect(() => {
     async function loadPets() {
@@ -90,50 +52,13 @@ export default function Dashboard() {
     <Container>
       <h1>início</h1>
       <Content>
-        <MultiSelect
-          name="Tipo"
-          options={[
-            { label: 'cachorro', value: 'dog' },
-            { label: 'gato', value: 'cat' },
-          ]}
-          onChange={onChangeFilters}
-        />
+        <Header />
         <>
           {loading ? (
             <Loading />
           ) : (
             <>
-              {pets.length > 0 ? (
-                <ul>
-                  {pets.map(pet => (
-                    <li key={pet.id}>
-                      <button type="submit">
-                        <MdFavorite size={15} />
-                      </button>
-                      <img src={pet.files[0]?.url || noImage} alt="" />
-                      <div>
-                        <TagsFilters>
-                          {pet.filters.map((filter, index) => (
-                            <TagFilter key={index} color={returnColor(filter)}>
-                              {returnTraduction(filter)}
-                            </TagFilter>
-                          ))}
-                        </TagsFilters>
-                        <strong>{pet.name}</strong>
-                        <p>
-                          <MdLocationOn color="#bb2929" size={18} />
-                          {pet.address.city} - {pet.address.state}
-                          <strong>(3.6 km)</strong>
-                        </p>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              ) : (
-                <div className="no-pets">
-                  <h2>ops, não temos pets cadastrados no momento</h2>
-                </div>
-              )}
+              <PetsList pets={pets} />
             </>
           )}
         </>
