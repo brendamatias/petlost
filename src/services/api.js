12d86a3633a73cf058d32f6e0359999a6abf383d
brendamatias <brendamatias.sobral@gmail.com>
2021-02-28 19:12:21 -0300

    config url

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import openSocket from 'socket.io-client';
 
 const api = axios.create({
-  // baseURL: 'https://petlost-api-dev.herokuapp.com',
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://petlost-api-dev.herokuapp.com',
+  // baseURL: 'http://localhost:3333',
 });
 
-const socket = openSocket('http://localhost:3333');
+const socket = openSocket('https://petlost-api-dev.herokuapp.com');
 
 export { api, socket };
